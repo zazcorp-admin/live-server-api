886de7b096b08b409233a85d1d3857d8e01a87a5
zazcorp-admin <zazhosting@gmail.com>
2021-11-19 12:27:51 +0600

    change method to generic

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from . import views 
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+router.register('item', views.ItemViewset, basename='item')
 
 urlpatterns = [
-	path('', views.index, name='home')
+	# path('', views.index, name='home')
+	path('', include(router.urls))
 ]
--- core/views.py
+++ core/views.py
@@ -5,11 +5,17 @@ from .models import Item
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.authentication import BasicAuthentication
+from rest_framework import viewsets
+from rest_framework import mixins
 
 # Create your views here.
 
-@api_view(['GET'])
-def index(request):
-	items = Item.objects.all()
-	serializer = ItemSerializer(items, many=True)
-	return Response(serializer.data)
+# @api_view(['GET'])
+# def index(request):
+# 	items = Item.objects.all()
+# 	serializer = ItemSerializer(items, many=True)
+# 	return Response(serializer.data)
+
+class ItemViewset(viewsets.GenericViewSet,mixins.CreateModelMixin, mixins.ListModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, mixins.RetrieveModelMixin):
+	serializer_class = ItemSerializer
+	queryset = Item.objects.all()
